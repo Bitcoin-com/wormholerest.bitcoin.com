47d0b5992bc74af0982ae73144eca1d0fef3f15f
Gabriel Cardona <gabriel@bitcoin.com>
2018-08-03 11:38:16 -0700

    PayloadCreation endpoints

--- routes/payloadCreation.js
+++ routes/payloadCreation.js
@@ -13,23 +13,24 @@ let username = process.env.RPC_USERNAME;
 let password = process.env.RPC_PASSWORD;
 
 let config = {
-  dataRetrievalRateLimit1: undefined,
-  dataRetrievalRateLimit2: undefined,
-  dataRetrievalRateLimit3: undefined,
-  dataRetrievalRateLimit4: undefined,
-  dataRetrievalRateLimit5: undefined,
-  dataRetrievalRateLimit6: undefined,
-  dataRetrievalRateLimit7: undefined,
-  dataRetrievalRateLimit8: undefined,
-  dataRetrievalRateLimit9: undefined,
-  dataRetrievalRateLimit10: undefined,
-  dataRetrievalRateLimit11: undefined,
-  dataRetrievalRateLimit12: undefined
+  payloadCreationRateLimit1: undefined,
+  payloadCreationRateLimit2: undefined,
+  payloadCreationRateLimit3: undefined,
+  payloadCreationRateLimit4: undefined,
+  payloadCreationRateLimit5: undefined,
+  payloadCreationRateLimit6: undefined,
+  payloadCreationRateLimit7: undefined,
+  payloadCreationRateLimit8: undefined,
+  payloadCreationRateLimit9: undefined,
+  payloadCreationRateLimit10: undefined,
+  payloadCreationRateLimit11: undefined,
+  payloadCreationRateLimit12: undefined,
+  payloadCreationRateLimit13: undefined
 };
 
 let i = 1;
-while(i < 19) {
-  config[`dataRetrievalRateLimit${i}`] = new RateLimit({
+while(i < 14) {
+  config[`payloadCreationRateLimit${i}`] = new RateLimit({
     windowMs: 60*60*1000, // 1 hour window
     delayMs: 0, // disable delaying - full speed until the max limit is reached
     max: 60, // start blocking after 60 requests
@@ -44,13 +45,12 @@ while(i < 19) {
   i++;
 }
 
-router.get('/', config.dataRetrievalRateLimit1, (req, res, next) => {
-  res.json({ status: 'dataRetrieval' });
+router.get('/', config.payloadCreationRateLimit1, (req, res, next) => {
+  res.json({ status: 'payloadCreation' });
 });
 
-router.post('/whcGetAllBalancesForAddress/:address', config.dataRetrievalRateLimit2, (req, res, next) => {
+router.post('/whcCreatePayloadBurnBCH', config.payloadCreationRateLimit2, (req, res, next) => {
 
-  console.log('sdasdasdfds')
   BitboxHTTP({
     method: 'post',
     auth: {
@@ -59,10 +59,307 @@ router.post('/whcGetAllBalancesForAddress/:address', config.dataRetrievalRateLim
     },
     data: {
       jsonrpc: "1.0",
-      id:"whc_getallbalancesforaddress",
-      method: "whc_getallbalancesforaddress",
+      id:"whc_createpayload_burnbch",
+      method: "whc_createpayload_burnbch"
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadChangeIssuer/:propertyId', config.payloadCreationRateLimit2, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_changeissuer",
+      method: "whc_createpayload_changeissuer",
       params: [
-        req.paams.address
+        req.paams.propertyId
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadCloseCrowdSale/:propertyId', config.payloadCreationRateLimit3, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_closecrowdsale",
+      method: "whc_createpayload_closecrowdsale",
+      params: [
+        req.paams.propertyId
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadGrant/:propertyId', config.payloadCreationRateLimit4, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_grant",
+      method: "whc_createpayload_grant",
+      params: [
+        req.paams.propertyId
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadIssuanceFixed/:ecosystem', config.payloadCreationRateLimit5, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_issuancefixed",
+      method: "whc_createpayload_issuancefixed",
+      params: [
+        req.paams.ecosystem
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadIssuanceCrowdsale/:ecosystem', config.payloadCreationRateLimit6, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_issuancecrowdsale",
+      method: "whc_createpayload_issuancecrowdsale",
+      params: [
+        req.paams.ecosystem
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadIssuanceFixed/:ecosystem', config.payloadCreationRateLimit7, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_issuancefixed",
+      method: "whc_createpayload_issuancefixed",
+      params: [
+        req.paams.ecosystem
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadIssuanceManaged/:ecosystem', config.payloadCreationRateLimit8, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_issuancemanaged",
+      method: "whc_createpayload_issuancemanaged",
+      params: [
+        req.paams.ecosystem
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadParticrwoSale/:amount', config.payloadCreationRateLimit9, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_particrwosale",
+      method: "whc_createpayload_particrwosale",
+      params: [
+        req.paams.amount
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadRevoke/:propertyId', config.payloadCreationRateLimit10, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_revoke",
+      method: "whc_createpayload_revoke",
+      params: [
+        req.paams.propertyId
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadSendAll/:ecosystem', config.payloadCreationRateLimit11, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_sendall",
+      method: "whc_createpayload_sendall",
+      params: [
+        req.paams.ecosystem
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadSimpleSend/:propertyId', config.payloadCreationRateLimit12, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_simplesend",
+      method: "whc_createpayload_simplesend",
+      params: [
+        req.paams.propertyId
+      ]
+    }
+  })
+  .then((response) => {
+    res.json(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
+  });
+});
+
+router.post('/whcCreatePayloadSTO/:propertyId', config.payloadCreationRateLimit13, (req, res, next) => {
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"whc_createpayload_sto",
+      method: "whc_createpayload_sto",
+      params: [
+        req.paams.propertyId
       ]
     }
   })
