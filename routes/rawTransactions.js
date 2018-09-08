0ef08c1094d4fbbeb8dc8a9bb8cc6fbc3ea5aa59
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-08 15:59:25 -0600

    Add sendRawTransactions endpoint.

--- routes/rawTransactions.js
+++ routes/rawTransactions.js
@@ -172,4 +172,80 @@ router.post('/create/:inputs/:outputs', config.rawTransactionsRateLimit7, async 
   }
 });
 
+router.post('/sendRawTransaction/:hex', config.rawTransactionsRateLimit5, (req, res, next) => {
+  try {
+    let transactions = JSON.parse(req.params.hex);
+    if(transactions.length > 20) {
+      res.json({
+        error: 'Array too large. Max 20 transactions'
+      });
+    }
+
+    let result = [];
+    transactions = transactions.map((transaction) => {
+      return WormholeHTTP({
+        method: 'post',
+        auth: {
+          username: username,
+          password: password
+        },
+        data: {
+          jsonrpc: "1.0",
+          id:"sendrawtransaction",
+          method: "sendrawtransaction",
+          params: [
+            transaction
+          ]
+        }
+      })
+      .catch(error => {
+        try {
+          return {
+            data: {
+              result: error.response.data.error.message
+            }
+          };
+        } catch (ex) {
+          return {
+            data: {
+              result: "unknown error"
+            }
+          };
+        }
+      })
+    })
+    axios.all(transactions)
+    .then(axios.spread((...args) => {
+      for (let i = 0; i < args.length; i++) {
+        let parsed = args[i].data.result;
+        result.push(parsed);
+      }
+      res.json(result);
+    }));
+  }
+  catch(error) {
+    WormholeHTTP({
+      method: 'post',
+      auth: {
+        username: username,
+        password: password
+      },
+      data: {
+        jsonrpc: "1.0",
+        id:"sendrawtransaction",
+        method: "sendrawtransaction",
+        params: [
+          req.params.hex
+        ]
+      }
+    })
+    .then((response) => {
+      res.json(response.data.result);
+    })
+    .catch((error) => {
+      res.send(error.response.data.error.message);
+    });
+  }
+});
+
 module.exports = router;
